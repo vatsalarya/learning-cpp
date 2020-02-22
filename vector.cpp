af7e7880f7f4fd448799e1bd499aa971bea9fdaa
vatsalarya <59337963+vatsalarya@users.noreply.github.com>
2020-02-22 10:04:57 +0530

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,85 +1,79 @@
+// Runs infinitely
+
 #include <iostream>
-#include<vector>
+#include <vector>
+#include <stdio.h>
 using namespace std;
 
 int main(){
    
    vector <int> numbers;
-   cout<<boolalpha;
-   bool complete{false};
-   while(complete=false){
-
+   int choice{};
+   do{
       cout<<"Please select one of the following options : \n"
-       <<"P - Print numbers\n" 
-       <<"A - Add a number\n"
-       <<"M - Display mean of the numbers\n"
-       <<"S - Display teh smallest number\n"
-       <<"L - Display the largest number\n"
-       <<"Q - Quit\n\n";
-       int choice{};
-       cin>>choice;
+          <<"P - Print numbers\n" 
+          <<"A - Add a number\n"
+          <<"M - Display mean of the numbers\n"
+          <<"S - Display teh smallest number\n"
+          <<"L - Display the largest number\n"
+          <<"Q - Quit\n\n";
+      cin>>choice;
 
-     if((choice=='p')||(choice=='P')){
-        if((numbers.size())==0)
+      if((choice=='p')||(choice=='P')){
+         if((numbers.size())==0)
             cout<<"[] - the list is empty.\n";           
-        else{
+         else{
             cout<<"[ ";
-            for(int i=0;i<numbers.size();i++)
-                cout<<numbers.at(i)<<" ";
+            for(auto number : numbers)
+                cout<<number<<" ";
             cout<<"]";
-        }
+         }
         
-     }
-     else if((choice=='a')||(choice=='A')){
+      }
+      else if((choice=='a')||(choice=='A')){
          cout<<"\nEnter a number to add to the list : ";
          int add_num{};
          cin>>add_num;
          numbers.push_back(add_num);
          cout<<add_num<<" added!\n";
-     }
-     else if((choice=='m')||(choice=='M')){
-        if(numbers.size()==0)
+      }
+      else if((choice=='m')||(choice=='M')){
+         if(numbers.size()==0)
             cout<<"Unable to calculate mean - no data.";
-        else{
-         int sum{};
-         for(int i=0;i<numbers.size();i++)
-            sum+=numbers.at(i);
-        cout<<"\nThe average of the numbers in the list is : "<<sum/numbers.size();
-        }
-     }
-     else if((choice=='s')||(choice=='S')){
-        if(numbers.size()==0)
+         else{
+            int sum{};
+            for(auto number : numbers)
+               sum+=number;
+         cout<<"\nThe average of the numbers in the list is : "<<sum/numbers.size();
+         }
+      }
+      else if((choice=='s')||(choice=='S')){
+         if(numbers.size()==0)
             cout<<"Unable to calculate smallest - no data.";
-        else{
-            int smallest{};
-            smallest=numbers.at(0);
-            for(int i=1;i<numbers.size();i++){
-            if(smallest>numbers.at(i))
-                smallest=numbers.at(i); 
+         else{
+            int smallest=numbers.at(0);
+            for(auto number : numbers){
+               if(smallest>number)
+                  smallest=number; 
             }
-        cout<<"The smallest number in the list is : "<<smallest;
-        }
+         cout<<"The smallest number in the list is : "<<smallest;
+         }
      }
 
      else if((choice=='l')||(choice=='L')){
           if(numbers.size()==0)
             cout<<"Unable to calculate largest - no data.";
-        else{
-            int largest{};
-            largest=numbers.at(0);
-            for(int i=1;i<numbers.size();i++){
-            if(largest<numbers.at(i))
-                largest=numbers.at(i);
-        }
-        cout<<"The largest number in the list is : "<<largest;
-        }
+         else{
+            int largest=numbers.at(0);
+            for(auto number : numbers){
+            if(largest<number)
+                largest=number;
+            }
+         cout<<"The largest number in the list is : "<<largest;
+         }
      }
-
-     else if((choice=='q')||(choice=='Q')){
-         complete = true;
-
-     }
-     else
-        cout<<"Unknown selection, please try again";
+      else
+         cout<<"Unknown selection, please try again";
    }
+      while((choice!='q')||(choice!='Q'));
 }
