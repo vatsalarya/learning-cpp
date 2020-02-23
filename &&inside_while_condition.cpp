4b4f542ea3fa9c3f045b7121d78d9bba8ad8253e
vatsalarya <59337963+vatsalarya@users.noreply.github.com>
2020-02-23 13:42:25 +0530

    Update &&inside_while_condition.cpp

--- &&inside_while_condition.cpp
+++ &&inside_while_condition.cpp
@@ -8,12 +8,22 @@ using namespace std;
 
 
 int main() {
-    int N,i=0;
-    std::cin>>N;
-    int *A = new int[N];
+    /* To print reverse*/   
+    size_t array_size{},i{};
+    cin>>array_size;
+    // THIS METHOD OF WHILE LOOP TAKES INPUT UNTIL THE >> IS GETTING CORRECT INPUT (IN THIS CASE INTEGER  )
+    int *A = new int[array_size];
     while(std::cin>>A[i++]);
-    while(std::cout<<A[--N]<<' ' && N);
-    cout<<N;
+    while(std::cout<<A[--array_size]<<' ' && array_size);
     delete[] A;
+    //my method
+    // size_t *ptr=new size_t[array_size];
+    // for(size_t i=0;i<array_size;++i)
+    //     cin>>ptr[i];
+    
+    // for(size_t i=0;i<array_size;++i)
+    //     cout<<ptr[array_size-i]<<" ";
+    // delete [] ptr;
+    
     return 0;
 }
